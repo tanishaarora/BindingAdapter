1310d5b0e78de8dde6576f3b7ac73444c4b2470e
Tanisha Arora <Tanisha.Arora@SRMTECHSOL.Local>
2023-10-26 15:22:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/bindingadapter/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bindingadapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.bindingadapter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val post = Post("Tanisha","Developer","https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")
+        binding.post = post
+    }
+}
---
+++ app/src/main/java/com/example/bindingadapter/Post.kt
@@ -0,0 +1,3 @@
+package com.example.bindingadapter
+
+data class Post(val title: String,val description: String, val url: String)
